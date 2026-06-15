0700012959e741f3f3d14f026d7c1d251753d630
FiF <2998919571@qq.com>
2026-06-15 11:00:44 +0800

    Create struct.cpp
    测试用的，

---
+++ struct.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct Student {
+    string name;
+    int age;
+    double gpa;
+};
+struct Teacher {
+    string name;
+    int age;
+    double salary;
+};
